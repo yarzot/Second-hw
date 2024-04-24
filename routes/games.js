6795148f05e408c7e07d9a9409a7c90dcb4ea3e1
yarzot <yarozot71@gmail.com>
2024-04-24 18:32:40 +0300

    bad

--- routes/games.js
+++
@@ -1,9 +0,0 @@
-const {sendAllGames, deleteGame, addGameController} = require("../controllers/games")
-const gamesRouter = require("express").Router(); // Создали роутер
-const { getAllGames } = require("../middlewares/games")
-
-gamesRouter.get("/games", getAllGames, sendAllGames)
-gamesRouter.delete("/games/:id", getAllGames, deleteGame);
-gamesRouter.post("/games", getAllGames, addGameController)
-
-module.exports = gamesRouter;
